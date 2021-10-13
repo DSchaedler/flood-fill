8f56532786b54288e56334562aed697036c5f099
D Schaedler <dominicschaedler@gmail.com>
2021-10-13 12:04:49 -0400

    made $pixels_holding into a hash for access speed.

--- app/main.rb
+++ app/main.rb
@@ -1,19 +1,20 @@
-PIXELS_PER_TICK = 4
+# frozen_string_literal: true
+
+PIXELS_PER_TICK = 500
 PIXEL_SIZE = 1
 
-def tick(args)
+def tick(args) # rubocop:disable Metrics/AbcSize
   args.gtk.log_level = :off
   args.render_target(:pixels).clear_before_render = false
 
   tick_zero(args) if args.state.tick_count.zero?
-
   flood_fill(args)
 
   args.outputs.sprites << { x: 0, y: 0, w: 1280, h: 720, path: :pixels }
   args.outputs.debug << args.gtk.framerate_diagnostics_primitives
 end
 
-def tick_zero(args)
+def tick_zero(args) # rubocop:disable Metrics/AbcSize
   $triangle ||= pick_vertecies(args)
   $lines ||= make_lines(args, $triangle)
   $center ||= get_center($triangle)
@@ -23,8 +24,9 @@ def tick_zero(args)
   bressenham(args, $lines[1])
   bressenham(args, $lines[2])
 
-  $pixels_holding ||= []
-  $pixels_holding << { x: $center[:x], y: $center[:y] }
+  $pixels_holding ||= {}
+  center = { x: $center[:x], y: $center[:y] }
+  $pixels_holding[center] = center
 
   $pixels.each do |key_x, value|
     value.each_key do |key_y|
@@ -43,14 +45,14 @@ def pick_vertecies(_args)
   point
 end
 
-def make_lines(_args, point)
+def make_lines(_args, point) # rubocop:disable Metrics/AbcSize
   lines = []
   lines << { x: point[0].x, y: point[0].y, x2: point[1].x, y2: point[1].y }
   lines << { x: point[1].x, y: point[1].y, x2: point[2].x, y2: point[2].y }
   lines << { x: point[2].x, y: point[2].y, x2: point[0].x, y2: point[0].y }
 end
 
-def get_center(point)
+def get_center(point) # rubocop:disable Metrics/AbcSize
   { x: ((point[0].x + point[1].x + point[2].x) / 3).to_i, y: ((point[0].y + point[1].y + point[2].y) / 3).to_i }
 end
 
@@ -90,7 +92,7 @@ def bressenham(_args, line)
     $pixels[pixel_x] ||= {}
     $pixels[pixel_x][pixel_y] = true
     error -= deltay
-    if error < 0
+    if error.negative?
       y += ystep
       error += deltax
     end
@@ -100,16 +102,18 @@ def bressenham(_args, line)
 end
 
 def flood_fill(args)
-  $pixels_to_check = $pixels_holding.dup
+  # $pixels_to_check = $pixels_holding.dup
 
   times = PIXELS_PER_TICK
   pixels_to_rt = []
 
-  return if $pixels_to_check.empty?
+  return if $pixels_holding.empty?
 
-  while times > 0
+  while times.positive?
 
-    pixel = $pixels_to_check[0]
+    break unless $pixels_holding.first
+
+    key, pixel = $pixels_holding.first
     $pixels[pixel[:x]] ||= {}
     $pixels[pixel[:x]][pixel[:y]] = true
     pixels_to_rt << { x: pixel[:x], y: pixel[:y], w: PIXEL_SIZE, h: PIXEL_SIZE }
@@ -118,19 +122,19 @@ def flood_fill(args)
 
     next_pixel0 = { x: pixel[:x] - PIXEL_SIZE, y: pixel[:y] }
     $pixels[next_pixel0[:x]] ||= {}
-    $pixels_holding << next_pixel0 unless $pixels[next_pixel0[:x]][next_pixel0[:y]]
+    $pixels_holding[next_pixel0] = next_pixel0 unless $pixels[next_pixel0[:x]][next_pixel0[:y]] || $pixels_holding.include?(next_pixel0)
 
     next_pixel1 = { x: pixel[:x] + PIXEL_SIZE, y: pixel[:y] }
     $pixels[next_pixel1[:x]] ||= {}
-    $pixels_holding << next_pixel1 unless $pixels[next_pixel1[:x]][next_pixel1[:y]]
+    $pixels_holding[next_pixel1] = next_pixel1 unless $pixels[next_pixel1[:x]][next_pixel1[:y]] || $pixels_holding.include?(next_pixel1)
 
     next_pixel2 = { x: pixel[:x], y: pixel[:y] - PIXEL_SIZE }
     $pixels[next_pixel2[:x]] ||= {}
-    $pixels_holding << next_pixel2 unless $pixels[next_pixel2[:x]][next_pixel2[:y]]
+    $pixels_holding[next_pixel2] = next_pixel2 unless $pixels[next_pixel2[:x]][next_pixel2[:y]] || $pixels_holding.include?(next_pixel2)
 
     next_pixel3 = { x: pixel[:x], y: pixel[:y] + PIXEL_SIZE }
     $pixels[next_pixel3[:x]] ||= {}
-    $pixels_holding << next_pixel3 unless $pixels[next_pixel3[:x]][next_pixel3[:y]]
+    $pixels_holding[next_pixel3] = next_pixel3 unless $pixels[next_pixel3[:x]][next_pixel3[:y]] || $pixels_holding.include?(next_pixel3)
 
     times -= 1
   end
